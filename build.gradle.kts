00b18273e4e871be71139643b64587cba4895910
Raghavendra <raghavendra.g@lowes.com>
2023-10-16 00:30:55 +0530

    KMM version catlog

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    `version-catalog`
+    `maven-publish`
+}
+
+group = properties("GROUP")
+version = properties("VERSION")
+
+catalog {
+    versionCatalog {
+        from(files("libs.versions.toml"))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["versionCatalog"])
+            groupId = properties("GROUP")
+            artifactId = properties("POM_ARTIFACT_ID")
+            version = properties("VERSION")
+
+            pom {
+                name.set(properties("POM_NAME"))
+                description.set(properties("POM_DESCRIPTION"))
+                url.set(properties("POM_URL"))
+                inceptionYear.set(properties("POM_INCEPTION_YEAR"))
+
+                licenses {
+                    license {
+                        name.set(properties("POM_LICENSE_NAME"))
+                        url.set(properties("POM_LICENSE_URL"))
+                        distribution.set(properties("POM_LICENSE_DIST"))
+                    }
+                }
+
+                scm {
+                    url.set(properties("POM_SCM_URL"))
+                    connection.set(properties("POM_SCM_CONNECTION"))
+                    developerConnection.set(properties("POM_SCM_DEV_CONNECTION"))
+                }
+
+                issueManagement {
+                    system.set(properties("POM_ISSUES_SYSTEM"))
+                    url.set(properties("POM_ISSUES"))
+                }
+            }
+        }
+    }
+}
